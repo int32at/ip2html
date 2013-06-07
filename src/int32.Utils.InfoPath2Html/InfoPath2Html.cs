bf41c8a30e09f4d989de946ff1712402b5394016
int32at <admin@int32.at>
2013-06-07 17:22:29 +0200

    init commit

---
+++ src/int32.Utils.InfoPath2Html/InfoPath2Html.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using System.Xml.XPath;
+using System.Xml.Xsl;
+
+namespace int32.Util
+{
+    public class InfoPath2Html
+    {
+        public string Generate(XmlDocument InfoPathData, XmlDocument XsltStyleSheet)
+        {
+            var xsltProcessor = new XslCompiledTransform();
+
+            using(var writer = new StringWriter())
+            using (var xmlWriter = new XmlTextWriter(writer))
+            {
+                XPathNavigator ipForm = InfoPathData.CreateNavigator();
+                xsltProcessor.Load(XsltStyleSheet);
+                xsltProcessor.Transform(InfoPathData, xmlWriter);
+
+                return writer.ToString();
+            }
+        }
+    }
+}
